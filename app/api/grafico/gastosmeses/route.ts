a148781c0763b99470ecad5f5de2477c722634f9
Jonatha Leite <jonathagleite@gmail.com>
2026-08-03 00:13:08 -0300

    More alterations

--- app/api/grafico/gastosmeses/route.ts
+++ app/api/grafico/gastosmeses/route.ts
@@ -10,6 +10,71 @@ export async function GET() {
   });
 
   // agrupar por categoria
+const agrupado: Record<
+string,
+number
+> = {};
+
+ const meses = [
+    "Jan",
+    "Fev",
+    "Mar",
+    "Abr",
+    "Mai",
+    "Jun",
+    "Jul",
+    "Ago",
+    "Set",
+    "Out",
+    "Nov",
+    "Dez"
+];
+
+  transacoes.forEach((t) => {
+  if (!t.data) return;
+
+   
+const mes = meses[
+    new Date(t.data).getMonth()
+];
+
+
+
+    if(!agrupado[mes]){
+
+        agrupado[mes]=0;
+
+    }
+
+    agrupado[mes]+=t.valor ?? 0;
+
+ 
+
+});
+const resultado =
+Object.entries(agrupado)
+.map(([name,value])=>({
+
+    name,
+    value
+
+}));
+return NextResponse.json(resultado);
+}
+
+
+/*import prisma from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const userId = 1; // depois você troca pelo usuário logado
+
+  const transacoes = await prisma.transacao.findMany({
+    where: { userId },
+   
+  });
+
+  // agrupar por categoria
 
   transacoes.forEach((t) => {
 
@@ -58,3 +123,4 @@ Object.entries(agrupado)
 
 });
 }
+*/
